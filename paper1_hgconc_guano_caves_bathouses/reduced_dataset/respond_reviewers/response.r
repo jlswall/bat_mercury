06261c984b18225ee5e7b9854ee955b9cb0e32c7
Jenise Swall <jenise.swall@gmail.com>
2019-04-08 20:05:55 -0400

    Minor changes.

--- paper1_hgconc_guano_caves_bathouses/reduced_dataset/respond_reviewers/response.r
+++ paper1_hgconc_guano_caves_bathouses/reduced_dataset/respond_reviewers/response.r
@@ -331,7 +331,7 @@ rm(myColors, xPosit, paramsPlot, caveNmPlot)
 
 
 #############################################
-## Examing just caves (no bat houses).
+## Examining just caves (no bat houses).
 
 ## Subset to only caves that have more than 2 observations.
 cavesDF <- subset(enoughObsDF, CaveOrHouse=="cave")
@@ -409,6 +409,7 @@ rm(caves.gls)
 #############################################
 
 
+
 #############################################
 ## Now, look at just bat houses (no caves).
 
@@ -461,6 +462,7 @@ x## ##########
 #############################################
 
 
+
 #############################################
 ## ##########
 ## Look at all locations with more than 2 observations, including both
@@ -493,6 +495,7 @@ pval.diff <- 2 * pt(-abs(est.diff/est.diff.sd), df=t.df)
 rm(diffTypeContr, est.diff, est.diff.sd)
 ## ##########
 #############################################
+
 
 
 
@@ -572,320 +575,3 @@ rm(avgCaveContr, est.avg, est.avg.sd)
 rm(caves.gls)
 ## ##########
 #############################################
-
-
-
-
-
-
-
-## Use gls with no intercept, so each cave will have its own estimate.
-glsfit <- gls(Mercury ~ -1 + Place, varIdent(form = ~1|Place), data=enoughObsDF)
-
-## Fit the contrast for the difference (avg caves - avg bat houses).
-diffTypeContr <- c(rep(1/6, 6), rep(-1/2, 2))  # 6 caves, 2 bat houses
-## Find the conf. interval.
-est.diff <- sum(diffTypeContr * coef(glsfit)) # t(c) %*% betahat
-est.diff.sd <- sqrt(as.numeric(t(diffTypeContr) %*% glsfit$varBeta %*% diffTypeContr))
-t.df <- nrow(enoughObsDF) - length(coef(glsfit))
-ci.diff <- est.diff + c(qt(0.025, df=t.df)*est.diff.sd, qt(0.975, df=t.df)*est.diff.sd)
-## CI is (-0.03452572,  0.11454729), or 0.04001079 +/- 0.07453651
-pval.diff <- 2 * pt(-abs(est.diff/est.diff.sd), df=t.df)
-## p-val: 0.2893798
-rm(diffTypeContr, est.diff, est.diff.sd)
-
-
-## ## Fit the contrast for the overall mean, assuming all locations equal.
-## muContr <- c(rep(1/8, 8))  # 6 caves, 2 bat houses
-## ## Find the conf. interval.
-## est.mu <- sum(muContr * coef(glsfit)) # This is t(c) %*% betahat
-## est.mu.sd <- sqrt(as.numeric(t(muContr) %*% glsfit$varBeta %*% muContr))
-## t.df <- nrow(enoughObsDF) - length(coef(glsfit))
-## ci.mu <- est.mu + c(qt(0.025, df=t.df)*est.mu.sd, qt(0.975, df=t.df)*est.mu.sd)
-## ## CI is (0.500763, 0.583674), or 0.5422185 +/- 0.04145553
-## pval.mu <- 2 * pt(-abs(est.mu/est.mu.sd), df=t.df)
-## ## p-val is very close to 0.
-## rm(muContr, est.mu, est.mu.sd, t.df)
-
-
-## Fit the function for the overall mean, with equal weight for caves
-## and bat houses.
-adjmuContr <- c(0.5*rep(1/6, 6), 0.5*rep(1/2, 2))  # 6 caves, 2 bat houses
-## Find the conf. interval.
-est.adjmu <- sum(adjmuContr * coef(glsfit)) # This is t(c) %*% betahat
-est.adjmu.sd <- sqrt(as.numeric(t(adjmuContr) %*% glsfit$varBeta %*% adjmuContr))
-t.df <- nrow(enoughObsDF) - length(coef(glsfit))
-ci.adjmu <- est.adjmu + c(qt(0.025, df=t.df)*est.adjmu.sd, qt(0.975, df=t.df)*est.adjmu.sd)
-## CI is (0.4949476, 0.5694841), or 0.5322158 +/-0.03726825
-pval.adjmu <- 2 * pt(-abs(est.mu/est.mu.sd), df=t.df)
-## p-val is very close to 0.
-rm(adjmuContr, est.adjmu, est.adjmu.sd, t.df)
-
-
-## Look at differences between specific caves.
-my.lsmeans <- lsmeans(glsfit, "Place")
-pairs(my.lsmeans)  ## To get p-values for signif. differences.
-my.lsmeans  ## To get CIs.
-siteCIs <- as.data.frame(confint(pairs(my.lsmeans)))##[,c(1,2,5,6)]
-## Climax Cave - Florida Caverns Old Indian Cave: -0.1970431 +/- 0.08532543
-## Climax Cave - Judge's Cave: -0.2080646 +/- 0.1053992
-## Suwannee NWR Bat House - UF Gainesville Bat House: 0.365379167 +/- 0.165567
-
-## http://r.789695.n4.nabble.com/unequal-variance-assumption-for-lme-mixed-effect-model-td828664.html
-## try1 <- gls(Mercury ~ CaveOrHouse, varIdent(form=~CaveOrHouse), data=enoughObsDF)  ## From nlme package
-#############################################
-
-
-
-
-
-
-## #############################################
-## Get confidence interval for all measurements taken together.
-
-with(allDF, hist(Mercury))
-with(allDF, t.test(Mercury))
-## (0.4903519, 0.5708774)
-with(allDF, mean(Mercury))
-with(allDF, qt(0.975, df=nrow(allDF)-1)*sd(Mercury)/sqrt(nrow(allDF)))
-## 0.5306147 +/- 0.04026272
-
-## Just for reference purposes, how would this confidence interval
-## change if we used only surface measurements (not the lower portion
-## of the cores)?
-## with(subset(allDF, distFromSurface==0), t.test(Mercury))
-## (0.4605022, 0.6010963)
-## nSurface <- nrow(subset(allDF, distFromSurface==0))
-## with(subset(allDF, distFromSurface==0), mean(Mercury))
-## with(subset(allDF, distFromSurface==0),
-##     qt(0.975, df=nSurface-1)*sd(Mercury)/sqrt(nSurface))
-## 0.5307993 +/- 0.07029703
-## rm(nSurface)
-## #############################################
-
-
-
-## #############################################
-## Get confidence interval for each mean separately.
-
-## For caves:
-caveDF <- subset(allDF, CaveOrHouse=="cave")
-with(caveDF, t.test(Mercury))
-## (0.5032717, 0.5914671)
-with(caveDF, mean(Mercury))
-with(caveDF, qt(0.975, df=nrow(caveDF)-1)*sd(Mercury)/sqrt(nrow(caveDF)))
-## 0.5473694 +/- 0.0440977
-rm(caveDF)
-
-
-## For bat houses:
-bathouseDF <- subset(allDF, CaveOrHouse=="bat house")
-with(bathouseDF, t.test(Mercury))
-##  (0.3414909, 0.5324796)
-with(bathouseDF, mean(Mercury))
-with(bathouseDF, qt(0.975, df=nrow(bathouseDF)-1)*sd(Mercury)/sqrt(nrow(bathouseDF)))
-## 0.4369853 +/- 0.09549435
-rm(bathouseDF)
-## #############################################
-
-
-
-## #############################################
-## Assess whether there is a difference in average Hg concentration
-## for caves vs. bat houses.
-
-## Make a figure with side-by-side boxplots for caves vs. bat houses.
-library("figdim")
-## Set up two colors of gray, one for boxplots with cave data and one
-## for boxplots with bat house data.  Will use these colors in
-## subsequent plots.
-myColors <- c(gray(0.8), gray(0.45))
-names(myColors) <- c("bat house", "cave")
-
-
-## Set up dimensions of the graphics file.
-init.fig.dimen(file="boxplot_caves_vs_houses.pdf", height=3.0, width=3.0,
-               cex.axis=0.75, cex.lab=0.75, cex.main=0.75, cex.sub=0.75,
-               mai=c(0.6, 0.5, 0.1, 0.1), tcl=-0.2)
-boxplot(Mercury ~ CaveOrHouse, data=allDF,
-        xlab="Location type", ylab="Mercury concentration (ppm)",
-        col=myColors, outcex=0.6)
-abline(h=seq(0, 2, by=0.25), col="lightgray", lty=3)
-## Call boxplot for second time so that we can see the boxes on top of
-## the reference lines.
-boxplot(Mercury ~ CaveOrHouse, data=allDF,
-        xlab="Location type", ylab="Mercury concentration (ppm)",
-        col=myColors, outcex=0.6, add=T)
-with(subset(allDF, CaveOrHouse=="bat house"),
-     text(1, 1.35, paste("n =", sum(!is.na(Mercury))), cex=0.7 ) )
-with(subset(allDF, CaveOrHouse=="cave"),
-     text(2, 1.35, paste("n =", sum(!is.na(Mercury))), cex=0.7 ) )
-dev.off()
-
-
-
-## Now, perform t test to see whether the means are significantly
-## different.
-t.test(Mercury ~ CaveOrHouse, data=allDF)
-## p-value = 0.03765
-## 95% CI for mean of bat houses - caves: (-0.213934425, -0.006833758)
-diff(tapply(allDF$Mercury, allDF$CaveOrHouse, mean))
-## -0.1103841 +/- 0.1035503
-## #############################################
-
-
-
-## #############################################
-## Test whether the mean concentrations are different among the caves.
-
-## Build a subset with just the caves (no bat houses).
-caveDF <- subset(allDF, CaveOrHouse=="cave")
-
-## Visualize the measurements by cave.
-ggplot(caveDF, aes(x=Place, y=Mercury, color=coreID)) +
-  geom_jitter(width=0.3) +
-  ## geom_point() +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
-
-## We only look at caves which have at least 3 measurements.
-enoughObsDF <- subset(caveDF, Place %in% names(table(caveDF$Place))[table(caveDF$Place) > 2])
-rm(caveDF)
-
-## Visualize the retained measurements by cave.
-ggplot(enoughObsDF, aes(x=Place, y=Mercury, color=coreID)) +
-  geom_jitter(width=0.2) +
-  ## geom_point() +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
-
-
-## Make boxplots of measurements by cave name.
-init.fig.dimen(file="boxplot_by_cavename.pdf", height=3.5, width=5.5,
-               cex.axis=0.75, cex.lab=0.75, cex.main=0.75, cex.sub=0.75,
-               mai=c(1.1, 0.5, 0.1, 0.1), tcl=-0.2)
-## Save the parameters of the boxplot as you plot it.
-paramsPlot <- boxplot(Mercury ~ Place, data=enoughObsDF,
-        xlab=NA, ylab="Mercury concentration (ppm)",
-        col=myColors["cave"], outcex=0.6, xaxt="n")
-## Put on reference lines.
-abline(h=seq(0, 2, by=0.25), col="lightgray", lty=3)
-## The cave names are so long that the x-axis has to be done
-## separately, with carriage returns in the naming strings.
-paramsPlot$names  ## See the names
-caveNmPlot <- c("Climax Cave",
-               "Cottondale\nCave",
-               "Florida Caverns \nOld Indian Cave",
-               "Jerome's \nBat Cave",
-               "Judge's Cave",
-               "Snead's Cave")
-axis(1, las=2, at=1:length(paramsPlot$names), labels=caveNmPlot)
-## Re-plot the boxes so that they appear on top of the reference lines.
-paramsPlot <- boxplot(Mercury ~ Place, data=enoughObsDF,
-        xlab=NA, ylab="Mercury concentration (ppm)",
-        col=myColors["cave"], outcex=0.6, xaxt="n", add=TRUE)
-## Add sample size for each box.
-text(1:length(paramsPlot$n), 1.35, paste("n =", paramsPlot$n), cex=0.7)
-dev.off()
-rm(paramsPlot, caveNmPlot)
-
-
-
-## Check whether variances are significantly different between caves.
-fligner.test(Mercury ~ as.factor(Place), data=enoughObsDF)
-library("car")
-leveneTest(Mercury ~ as.factor(Place), data=enoughObsDF)
-## The above tests indicate that variances are likely unequal, so we
-## run Welch's ANOVA.
-oneway.test(Mercury ~ as.factor(Place), data=enoughObsDF, var.equal=FALSE)
-## Use the Games-Howell test.
-library("userfriendlyscience")
-oneway(y=enoughObsDF$Mercury, x=as.factor(enoughObsDF$Place), posthoc="games-howell")
-resDF <- posthocTGH(y=enoughObsDF$Mercury, x=as.factor(enoughObsDF$Place), method="games-howell")[["output"]][["games.howell"]]
-## Significant differences between:
-## Florida Caverns Old Indian Cave-Climax Cave:
-##   (0.1140023, 0.2800839)  or 0.197043095 +/- 0.0830408
-## Judge's Cave-Climax Cave:
-##   (0.1037451, 0.3123842)  or 0.208064649 +/- 0.1043195
-
-
-## To use standard ANOVA, with Tukey's multiple comparison technique:
-## myAOV <- aov(Mercury ~ Place, data=enoughObsDF)
-## summary(myAOV)
-## TukeyHSD(myAOV)
-## par(mar=c(5, 25, 4, 1))
-## plot(TukeyHSD(myAOV), las=1)
-## Significant differences between:
-## Climax Cave and Florida Caverns Old Indian Cave
-## Climax Cave and Jerome's Bat Cave
-## Climax Cave and Judge's Cave
-
-rm(enoughObsDF)
-## #############################################
-
-
-
-
-
-## #############################################
-## Test whether the mean concentrations are different among the bat
-## houses.
-
-## Build a subset with just the bat houses (no caves).
-bathouseDF <- subset(allDF, CaveOrHouse=="bat house")
-
-## Visualize the measurements by bat house.
-ggplot(bathouseDF, aes(x=Place, y=Mercury, color=coreID)) +
-  geom_jitter(width=0.1) +
-  ## geom_point() +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
-
-
-
-## Make boxplots of concentrations by bat house.
-init.fig.dimen(file="boxplot_by_bathousename.pdf", height=3.5, width=3.0,
-               cex.axis=0.75, cex.lab=0.75, cex.main=0.75, cex.sub=0.75,
-               mai=c(1.1, 0.5, 0.1, 0.1), tcl=-0.2)
-## Save the parameters of the boxplot as you plot it.
-paramsPlot <- boxplot(Mercury ~ Place, data=bathouseDF,
-        xlab=NA, ylab="Mercury concentration (ppm)",
-        col=myColors["bat house"], outcex=0.6, xaxt="n",
-        ylim=c(0, 1))
-## Put on reference lines.
-abline(h=seq(0, 2, by=0.25), col="lightgray", lty=3)
-## The cave names are so long that the x-axis has to be done
-## separately, with carriage returns in the naming strings.
-paramsPlot$names  ## See the names
-houseNmPlot <- c("Lower Suwannee \nNWR Bat House",
-               "UF Gainesville \nBat House")
-axis(1, las=2, at=1:length(paramsPlot$names), labels=houseNmPlot)
-## Re-plot the boxes so that they appear on top of the reference lines.
-paramsPlot <- boxplot(Mercury ~ Place, data=bathouseDF,
-        xlab=NA, ylab="Mercury concentration (ppm)",
-        col=myColors["bat house"], outcex=0.6, xaxt="n",
-        ylim=c(0, 1), add=TRUE)
-## Add sample size for each box.
-text(1:length(paramsPlot$n), 0.93, paste("n =", paramsPlot$n), cex=0.7)
-dev.off()
-rm(paramsPlot, houseNmPlot)
-
-
-
-t.test(Mercury ~ Place, data=bathouseDF)
-## p-value = 0.001358
-## CI for Suwanee Bat House - UF Gainesville Bat House
-## (0.2248231, 0.5059352)
-
-
-## What about using the Wilcoxon-Mann-Whitney test, since the samples
-## here are small?
-library("coin")
-wilcox_test(Mercury ~ as.factor(Place), data=bathouseDF, conf.level=0.95, distribution="exact")
-## p-value = 0.0003232
-## For asymptotic version of the test:
-## wilcox_test(Mercury ~ as.factor(Place), data=bathouseDF, conf.level=0.95)
-## p-value = 0.001565
-
-rm(bathouseDF)
-## #############################################
-
-
-
